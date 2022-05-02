57ea506f620587f9f08a032f4130f3dd2fcf3102
zhangyang <zhangyang@moqing.com>
2022-05-02 18:42:23 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  lintOnSave: true,
+  productionSourceMap: false,
+  // assetsDir: 'static',
+  devServer: {
+    host: 'admin.zy.com',
+    port: 8080,
+    proxy: {
+      '/cms': {
+        target: 'http://api.zy.com', //代理地址，这里设置的地址会代替axios中设置的baseURL
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        //ws: true,         // proxy websockets
+        pathRewrite: {
+          //pathRewrite方法重写url /api/test ==> /test
+          '^/cms': '',
+        },
+      },
+    },
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('lin', resolve('src/lin'))
+      .set('assets', resolve('src/asset'))
+    config.module
+      .rule('md')
+      .test(/\.md$/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.json', '.vue', '.scss', '.html'],
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: '@import "@/assets/style/share.scss";',
+      },
+    },
+  },
+  // node_modules依赖项es6语法未转换问题
+  transpileDependencies: ['vuex-persist'],
+}
