f8aae419cea42c5fadd2211a59768ba2fc708252
zhangyang <zhangyang@moqing.com>
2022-05-24 18:49:10 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     port: 8080,
     proxy: {
       '/cms': {
-        target: 'http://api.zy.com', //代理地址，这里设置的地址会代替axios中设置的baseURL
+        target: 'http://api.admin.zy.com', //代理地址，这里设置的地址会代替axios中设置的baseURL
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         //ws: true,         // proxy websockets
         pathRewrite: {
